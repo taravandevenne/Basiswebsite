d2b778857b30fe0dc8d88997c5acf7f71399c0a1
Tara Vandevenne <taravandevenne@hotmail.com>
2019-06-03 13:53:47 +0200

    initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Tara Vandevenne',
+            'email' => 'taravandevenne@hotmail.com',
+            'phone' => '0498846106',
+            'role_id' => '1',
+            'password' => '123456'
+        ]);
+    }
+}
